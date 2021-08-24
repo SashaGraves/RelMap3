42417fababd2feca1d46f2c78c5b447d5a2ec21b
sashagraves <all-exa@ya.ru>
2021-08-24 08:42:26 +0300

    configs

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          screens: './src/screens',
+          images: './src/images',
+          constants: './src/constants',
+          common: './src/common',
+        },
+      },
+    ],
+  ],
 };
